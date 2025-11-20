9856eaa1bd5cd20a5db8ffc18d99c272f0941020
Joel Tristan Peter Napier <ST10457625@vcconnect.edu.za>
2025-11-20 18:35:31 +0200

    Added the code to program cs to allow the usage of services and sessions. Added the connection string to the local database

--- PROG POE Part 1/Program.cs
+++ PROG POE Part 1/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using PROG_POE_Part_1.Data;
 using PROG_POE_Part_1.Services;
 
 namespace PROG_POE_Part_1
@@ -8,18 +10,28 @@ namespace PROG_POE_Part_1
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Register DbContext
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register services for DI
+            builder.Services.AddScoped<FileEncryptionService>();
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<ClaimService>();
+
+            // Add MVC support
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddScoped<FileEncryptionService>();
+            // Add session support
+            builder.Services.AddSession();
+            builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure middleware
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -28,8 +40,11 @@ namespace PROG_POE_Part_1
 
             app.UseRouting();
 
+            app.UseSession();
+
             app.UseAuthorization();
 
+            // Map default controller route
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
